b295e95ec1c16c82e22b6387b0a3032da083d424
khinsuwai97 <khinsuwai009@gmail.com>
2026-01-11 18:10:42 +0700

    initial set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-sand-100 text-forest-800 font-[--font-body] p-6">
+      App
+    </div>
+  );
+};
+
+export default App;
